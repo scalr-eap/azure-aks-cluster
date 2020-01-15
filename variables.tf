5806913bea491d6cf2cd60c789c531b65e7f8b96
Ryan <ryan.fee625@gmail.com>
2020-01-15 09:46:28 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "The Azure region of the cluster"
+}
+
+variable "cluster_name" {
+  description = "The name of the AKS cluster"
+}
+
+variable "resource_group" {
+  description = "The name of the existing resource group"
+}
+
+variable "cluster_dns_prefix" {
+  description = "The DNS prefix for the AKS cluster"
+}
+
+variable "node_pool_name" {
+  description = "The node pool name"
+}
+
+variable "node_count" {
+  description = "The number of nodes"
+}
+
+variable "node_size" {
+  description = "The instance size of the nodes"
+}
+
+variable "client_id" {
+  description = "The client id to be used for the cluster creation"
+}
+
+variable "client_secret" {
+  description = "The client secret to be used for the cluster creation"
+}
